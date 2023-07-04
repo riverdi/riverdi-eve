9faf22a81a322f688a086e82727e2479b7be6fde
Lukasz Skalski <contact@lukasz-skalski.com>
2023-07-04 14:35:20 +0200

    Set pixel precision for high-resolution IPS displays.
    The maximum range of coordinates for VERTEX2F
    depends on pixel precision defined by VERTEX_FORMAT
    command. By default it is 1/16 pixel precision, so
    that max. VERTEX2F range is -1024 to 1023 - not
    enough for IPS 7.0 and 10.1 inches display

--- riverdi-demo.c
+++ riverdi-demo.c
@@ -47,7 +47,7 @@ static void
 SAMAPP_GPU_Ball_Stencil()
 {
   int16_t xball = (DispWidth/2),yball = (DispHeight/2),rball = (DispWidth/8);
-  int16_t numpoints = 6,numlines = 8,i,asize,aradius,gridsize = 20;
+  int16_t pixel_precision, numpoints = 6,numlines = 8,i,asize,aradius,gridsize = 20;
   int32_t asmooth,dispr = (DispWidth - 10),displ = 10,dispa = 10,dispb = (DispHeight - 10),xflag = 1,yflag = 1;
 
   dispr -= ((dispr - displ)%gridsize);
@@ -83,27 +83,37 @@ SAMAPP_GPU_Ball_Stencil()
       App_WrDl_Buffer(phost, STENCIL_OP(INCR,INCR) );
       App_WrDl_Buffer(phost, COLOR_RGB(0, 0, 0) );
 
+      /* set the precision of VERTEX2F coordinates */
+#if defined (IPS_70) || (IPS_101)
+      /* VERTEX2F range: -2048 to 2047 */
+      App_WrDl_Buffer(phost, VERTEX_FORMAT(3));
+      pixel_precision = 8;
+#else
+      /* use default VERTEX_FORMAT(3) with VERTEX2F range: -1024 to 1023 */
+      pixel_precision = 16;
+#endif
+
       /* draw grid */
-      App_WrDl_Buffer(phost, LINE_WIDTH(16));
+      App_WrDl_Buffer(phost, LINE_WIDTH(pixel_precision));
       App_WrDl_Buffer(phost, BEGIN(LINES));
 
       for(i=0;i<=((dispr - displ)/gridsize);i++)
         {
-          App_WrDl_Buffer(phost, VERTEX2F((displ + i*gridsize)*16,dispa*16));
-          App_WrDl_Buffer(phost, VERTEX2F((displ + i*gridsize)*16,dispb*16));
+          App_WrDl_Buffer(phost, VERTEX2F((displ + i*gridsize)*pixel_precision,dispa*pixel_precision));
+          App_WrDl_Buffer(phost, VERTEX2F((displ + i*gridsize)*pixel_precision,dispb*pixel_precision));
         }
 
       for(i=0;i<=((dispb - dispa)/gridsize);i++)
         {
-          App_WrDl_Buffer(phost, VERTEX2F(displ*16,(dispa + i*gridsize)*16));
-          App_WrDl_Buffer(phost, VERTEX2F(dispr*16,(dispa + i*gridsize)*16));
+          App_WrDl_Buffer(phost, VERTEX2F(displ*pixel_precision,(dispa + i*gridsize)*pixel_precision));
+          App_WrDl_Buffer(phost, VERTEX2F(dispr*pixel_precision,(dispa + i*gridsize)*pixel_precision));
         }
 
       App_WrDl_Buffer(phost, END());
       App_WrDl_Buffer(phost, COLOR_MASK(0,0,0,0) );
-      App_WrDl_Buffer(phost, POINT_SIZE(rball*16) );
+      App_WrDl_Buffer(phost, POINT_SIZE(rball*16));
       App_WrDl_Buffer(phost, BEGIN(FTPOINTS));
-      App_WrDl_Buffer(phost, VERTEX2F(xball*16,yball*16));
+      App_WrDl_Buffer(phost, VERTEX2F(xball*pixel_precision,yball*pixel_precision));
       App_WrDl_Buffer(phost, STENCIL_OP(INCR,ZERO) );
       App_WrDl_Buffer(phost, STENCIL_FUNC(GEQUAL,1,255));
 
@@ -118,8 +128,8 @@ SAMAPP_GPU_Ball_Stencil()
 
               tempsmooth = asmooth - rball;
               aradius = (rball*rball + tempsmooth*tempsmooth)/(2*tempsmooth);
-              App_WrDl_Buffer(phost, POINT_SIZE(aradius*16) );
-              App_WrDl_Buffer(phost, VERTEX2F((xball - aradius + tempsmooth)*16,yball*16));
+              App_WrDl_Buffer(phost, POINT_SIZE(aradius*16));
+              App_WrDl_Buffer(phost, VERTEX2F((xball - aradius + tempsmooth)*pixel_precision,yball*pixel_precision));
             }
           else
             {
@@ -127,8 +137,8 @@ SAMAPP_GPU_Ball_Stencil()
 
               tempsmooth = rball - asmooth;
               aradius = (rball*rball + tempsmooth*tempsmooth)/(2*tempsmooth);
-              App_WrDl_Buffer(phost, POINT_SIZE(aradius*16) );
-              App_WrDl_Buffer(phost, VERTEX2F((xball+ aradius - tempsmooth)*16,yball*16));
+              App_WrDl_Buffer(phost, POINT_SIZE(aradius*16));
+              App_WrDl_Buffer(phost, VERTEX2F((xball+ aradius - tempsmooth)*pixel_precision,yball*pixel_precision));
             }
           }
 
@@ -141,28 +151,28 @@ SAMAPP_GPU_Ball_Stencil()
             asize = (i*rball*2/numlines);
             asmooth = (int16_t)smoothlerp((float)((float)(asize)/(2*(float)rball)),0,2*(float)rball);
             App_WrDl_Buffer(phost, LINE_WIDTH(asmooth * 16));
-            App_WrDl_Buffer(phost, VERTEX2F((xball - rball)*16,(yball - rball )*16));
-            App_WrDl_Buffer(phost, VERTEX2F((xball + rball)*16,(yball - rball )*16));
+            App_WrDl_Buffer(phost, VERTEX2F((xball - rball)*pixel_precision,(yball - rball)*pixel_precision));
+            App_WrDl_Buffer(phost, VERTEX2F((xball + rball)*pixel_precision,(yball - rball)*pixel_precision));
           }
         App_WrDl_Buffer(phost, END());
 
         App_WrDl_Buffer(phost, COLOR_MASK(1,1,1,1) );//enable all the colors
         App_WrDl_Buffer(phost, STENCIL_FUNC(ALWAYS,1,255));
         App_WrDl_Buffer(phost, STENCIL_OP(KEEP,KEEP));
-        App_WrDl_Buffer(phost, COLOR_RGB(255, 255, 255) );
-        App_WrDl_Buffer(phost, POINT_SIZE(rball*16) );
+        App_WrDl_Buffer(phost, COLOR_RGB(255, 255, 255));
+        App_WrDl_Buffer(phost, POINT_SIZE(rball*16));
         App_WrDl_Buffer(phost, BEGIN(FTPOINTS));
-        App_WrDl_Buffer(phost, VERTEX2F((xball - 1)*16,(yball - 1)*16));
-        App_WrDl_Buffer(phost, COLOR_RGB(0, 0, 0) );//shadow
-        App_WrDl_Buffer(phost, COLOR_A(160) );
-        App_WrDl_Buffer(phost, VERTEX2F((xball+16)*16,(yball+8)*16));
-        App_WrDl_Buffer(phost, COLOR_A(255) );
+        App_WrDl_Buffer(phost, VERTEX2F((xball - 1)*pixel_precision,(yball - 1)*pixel_precision));
+        App_WrDl_Buffer(phost, COLOR_RGB(0, 0, 0));//shadow
+        App_WrDl_Buffer(phost, COLOR_A(160));
+        App_WrDl_Buffer(phost, VERTEX2F((xball+pixel_precision)*pixel_precision,(yball+8)*pixel_precision));
+        App_WrDl_Buffer(phost, COLOR_A(255));
         App_WrDl_Buffer(phost, COLOR_RGB(255, 255, 255) );
-        App_WrDl_Buffer(phost, VERTEX2F(xball*16,yball*16));
+        App_WrDl_Buffer(phost, VERTEX2F(xball*pixel_precision,yball*pixel_precision));
         App_WrDl_Buffer(phost, COLOR_RGB(255, 0, 0) );
         App_WrDl_Buffer(phost, STENCIL_FUNC(GEQUAL,1,1));
         App_WrDl_Buffer(phost, STENCIL_OP(KEEP,KEEP));
-        App_WrDl_Buffer(phost, VERTEX2F(xball*16,yball*16));
+        App_WrDl_Buffer(phost, VERTEX2F(xball*pixel_precision,yball*pixel_precision));
 
         App_WrDl_Buffer(phost, END());
         App_WrDl_Buffer(phost,DISPLAY());
